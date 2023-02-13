029d221759f2aa354c6b8927a009eeb9a8a77f85
ASato <kikopam215@gmail.com>
2023-02-12 19:30:48 -0500

    Design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Button, Center, Container, Title } from '@mantine/core';
+import { Button, Center, Container, Title,Text,Card } from '@mantine/core';
 
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { Lifter } from '../components/Lifter/Lifter';
@@ -28,18 +28,28 @@ export default function HomePage() {
     setId(id + 1);
   };
 
+  const initialInstructions = ()=>{  
+    if(lifters.length==0){
+      return <Card mt="md">Add your lifters and fill in their lifts (at least their squat and bench press) to show predictive data.</Card>
+    }
+  }
+
   return (
     <>
       <ColorSchemeToggle />
       <Center>
         <Container m="lg">
-          <Title order={3}>Accumulated Total</Title>
+        <Title order={2}>Meet<Text span fw={400} inherit>Day</Text></Title>
+          <Title order={3}>📈 Accumulated Total</Title>
           <Button mt="lg" variant="light" onClick={addLifter}>
             + lifter
           </Button>
           <Button mt="lg" variant="light" ml="lg" color="green">
             + guess
           </Button>
+          <>
+          {initialInstructions()}
+          </>
           {lifters.map((lifter, idx) => (
             <Lifter key={idx} lifter={lifter} lifters={lifters} setLifters={setLifters} id={idx} />
           ))}
